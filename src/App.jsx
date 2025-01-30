e1c9cf2b4510dc35a50472586fd311069477657b
Amirsalar <Amirsalarmehrdad22@gmail.com>
2025-01-30 21:04:04 +0300

    Ui of the form is completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,53 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="form">
+      <div className="container">
+        <form action="">
+          <h2>Contact Us</h2>
+          <div className="fullname">
+            <div className="fieldsname">
+              <label htmlFor="">First Name *</label>
+              <input type="text" required/>
+            </div>
+            <div className="fieldsname">
+              <label htmlFor="">Last Name *</label>
+              <input type="text" required/>
+            </div>
+          </div>
+          <div className='fieldsname'><label htmlFor="">Email Adress *</label>
+          <input type="email" required/></div>
+          <div className='query'>
+            <label htmlFor="">Query Type</label>
+            <div className="radio-group">
+              <div><label htmlFor="">
+              <input type="radio" />General Enquiry
+              </label></div>
+              <div><label htmlFor="">
+              <input type="radio" />Support Request</label></div>
+              
+              </div>
+          </div>
+          <div className='fieldsname'>
+            <label htmlFor="">Message *</label>
+            <textarea name="" id="" rows={8} cols={40} required></textarea>
+          </div>
+          <div className="consent">
+            <label htmlFor="">
+              <input type="checkbox" name="" id="" />
+              I consent to being contacted by the team *
+            </label>
+          </div>
+          <button type='submit'>Submit</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
